c0080fe44856c44c80036aa0c2f91fb71e4a59f2
Vili Heikkilä <vili.heikkila@solita.fi>
2020-04-14 16:24:43 +0300

    initial version of python template

---
+++ python/main.py
@@ -0,0 +1,4 @@
+from src.exercise.hello import hello_world
+
+if __name__ == "__main__":
+    hello_world()
---
+++ python/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="exercise", packages=find_packages())
